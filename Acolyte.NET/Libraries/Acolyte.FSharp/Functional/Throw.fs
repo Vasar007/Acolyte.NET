194fb1b781d7cc175782c5c2587a0396a829c6dc
Vasily Vasilyev <vasar007@yandex.ru>
2020-01-04 19:59:49 +0300

    Add special methods to check values for F#

---
+++ Acolyte.NET/Libraries/Acolyte.FSharp/Functional/Throw.fs
@@ -0,0 +1,12 @@
+﻿module Acolyte.Functional.Throw
+
+open Acolyte.Assertions
+
+
+let ifNullValue value (paramName: string) (assertOnPureValueTypes: bool) =
+    paramName.ThrowIfNull("paramName") |> ignore // Replace with nameof operator which still does not compile now.
+    value.ThrowIfNullValue(paramName, assertOnPureValueTypes) |> ignore
+
+let ifNull obj (paramName: string) =
+    paramName.ThrowIfNull("paramName") |> ignore // Replace with nameof operator which still does not compile now.
+    obj.ThrowIfNull(paramName) |> ignore
