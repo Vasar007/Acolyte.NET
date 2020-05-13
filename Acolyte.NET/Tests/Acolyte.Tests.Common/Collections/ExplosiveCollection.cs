ef0cdf24e2800b283b4ab45276fae42e09ac76cf
Vasily Vasilyev <vasar007@yandex.ru>
2020-05-13 03:27:03 +0300

    Add explosive collection and enumerator for test puproses

--- Acolyte.NET/Tests/Acolyte.Tests.Common/Collections/ExplosiveCollection.cs
+++ Acolyte.NET/Tests/Acolyte.Tests.Common/Collections/ExplosiveCollection.cs
@@ -1,13 +1,58 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
+using Acolyte.Assertions;
 
 namespace Acolyte.Tests.Collections
 {
-    internal sealed class ExplosiveCollection
+    public sealed class ExplosiveCollection<T> : IEnumerable<T>, IEnumerable
     {
-        public ExplosiveCollection()
+        private readonly IEnumerable<T> _originalCollection;
+
+        private readonly int _explosiveIndex;
+
+
+        public ExplosiveCollection(
+            IEnumerable<T> originalCollection,
+            int explosiveIndex)
         {
+            _originalCollection = originalCollection.ThrowIfNull(nameof(originalCollection));
+            _explosiveIndex =
+                explosiveIndex.ThrowIfValueIsOutOfRange(nameof(explosiveIndex), 0, int.MaxValue);
+        }
+
+        #region IEnumerable<T> Implementation
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ExplosiveEnumerator.Create(
+                _originalCollection.GetEnumerator(),
+                _explosiveIndex
+            );
+        }
+
+        #endregion
+
+        #region IEnumerable Implementation
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+    }
+
+    public static class ExplosiveCollection
+    {
+        public static ExplosiveCollection<T> Create<T>(
+            IEnumerable<T> originalCollection,
+            int explosiveIndex)
+        {
+            return new ExplosiveCollection<T>(
+                originalCollection: originalCollection,
+                explosiveIndex: explosiveIndex
+            );
         }
     }
 }
---
+++ Acolyte.NET/Tests/Acolyte.Tests.Common/Collections/ExplosiveEnumerator.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Acolyte.Assertions;
+using Acolyte.Common;
+using Acolyte.Exceptions;
+
+namespace Acolyte.Tests.Collections
+{
+    public sealed class ExplosiveEnumerator<T> : IEnumerator<T>, IEnumerator
+    {
+        private readonly IEnumerator<T> _originalEnumerator;
+
+        private readonly int _explosiveIndex;
+
+        private bool _disposed;
+
+        private int _currentIndex;
+
+        [AllowNull, MaybeNull]
+        public T Current { get; private set; }
+
+        object? IEnumerator.Current => Current;
+
+
+        public ExplosiveEnumerator(
+            IEnumerator<T> originalEnumerator,
+            int explosiveIndex)
+        {
+            _originalEnumerator = originalEnumerator.ThrowIfNull(nameof(originalEnumerator));
+            _explosiveIndex =
+                explosiveIndex.ThrowIfValueIsOutOfRange(nameof(explosiveIndex), 0, int.MaxValue);
+
+            Current = default;
+            _currentIndex = Constants.NotFoundIndex;
+        }
+
+        public bool MoveNext()
+        {
+            if (_currentIndex == _explosiveIndex) throw new ExplosiveException(_explosiveIndex);
+
+            if (!_originalEnumerator.MoveNext()) return false;
+
+            ++_currentIndex;
+            Current = _originalEnumerator.Current;
+            return true;
+        }
+
+        public void Reset()
+        {
+            _originalEnumerator.Reset();
+        }
+
+        #region IDisposable Implementation
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+
+            _originalEnumerator.Dispose();
+
+            _disposed = true;
+        }
+
+        #endregion
+    }
+
+
+    public static class ExplosiveEnumerator
+    {
+        public static ExplosiveEnumerator<T> Create<T>(
+            IEnumerator<T> originalEnumerator,
+            int explosiveIndex)
+        {
+            return new ExplosiveEnumerator<T>(
+                originalEnumerator: originalEnumerator,
+                explosiveIndex: explosiveIndex
+            );
+        }
+    }
+}
