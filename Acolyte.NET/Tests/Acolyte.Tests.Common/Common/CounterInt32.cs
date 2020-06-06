3ae22e817eb98f4c54b86f1aef6e9a57629f35b3
Vasily Vasilyev <vasar007@yandex.ru>
2020-06-06 18:16:33 +0300

    Split counter classes

---
+++ Acolyte.NET/Tests/Acolyte.Tests.Common/Common/CounterInt32.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading;
+
+namespace Acolyte.Tests.Common
+{
+    public sealed class CounterInt32
+    {
+        private int _counter;
+
+        // Read Int32 is atomic operation on both x86 and x64.
+        public int Value => _counter;
+
+
+        public CounterInt32()
+        {
+            _counter = default;
+        }
+
+        public CounterInt32(int startValue)
+        {
+            _counter = startValue;
+        }
+
+        public CounterInt32 Increment()
+        {
+            Interlocked.Increment(ref _counter);
+            return this;
+        }
+
+        public int ExchangeIncrement()
+        {
+            return Interlocked.Exchange(ref _counter, _counter + 1);
+        }
+
+        public CounterInt32 Decrement()
+        {
+            Interlocked.Decrement(ref _counter);
+            return this;
+        }
+
+        public int ExchangeDecrement()
+        {
+            return Interlocked.Exchange(ref _counter, _counter - 1);
+        }
+
+        public CounterInt32 Reset()
+        {
+            return Reset(default);
+        }
+
+        public CounterInt32 Reset(int value)
+        {
+            Interlocked.Exchange(ref _counter, value);
+            return this;
+        }
+
+        public static implicit operator int(CounterInt32 counter)
+        {
+            return counter.Value;
+        }
+
+        public static CounterInt32 operator ++(CounterInt32 counter)
+        {
+            return counter.Increment();
+        }
+
+        public static CounterInt32 operator --(CounterInt32 counter)
+        {
+            return counter.Decrement();
+        }
+    }
+}
