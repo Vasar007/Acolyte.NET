ffb0c944d721fc53d4cd3fb0755d3efb10d02663
Vasily Vasilyev <vasar007@yandex.ru>
2020-09-28 17:46:40 +0300

    Add TwoWayDictionary collection

---
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Collections/TwoWayDictionary.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Acolyte.Collections
+{
+    public sealed class TwoWayDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    {
+        private readonly IDictionary<TKey, TValue> _fwdDictionary;
+        private readonly IDictionary<TValue, TKey> _revDictionary;
+
+        public TwoWayDictionary()
+            : this((IEqualityComparer<TKey>?) null, (IEqualityComparer<TValue>?) null)
+        {
+        }
+
+        public TwoWayDictionary(
+            IEqualityComparer<TKey> keyComparer)
+            : this(keyComparer, null)
+        {
+        }
+
+        public TwoWayDictionary(
+            IEqualityComparer<TValue> valueComparer)
+            : this(null, valueComparer)
+        {
+        }
+
+        public TwoWayDictionary(
+            IEqualityComparer<TKey>? keyComparer,
+            IEqualityComparer<TValue>? valueComparer)
+            : this(new Dictionary<TKey, TValue>(keyComparer), new Dictionary<TValue, TKey>(valueComparer))
+        {
+        }
+
+        private TwoWayDictionary(
+            IDictionary<TKey, TValue> fwdDictionary,
+            IDictionary<TValue, TKey> revDictionary)
+        {
+            _fwdDictionary = fwdDictionary;
+            _revDictionary = revDictionary;
+        }
+
+        public TValue this[TKey key] { get => _fwdDictionary[key]; }
+        public TKey this[TValue val] { get => _revDictionary[val]; }
+
+        public IReadOnlyList<TKey> GetKeys() => new List<TKey>(_fwdDictionary.Keys);
+        public IReadOnlyList<TValue> GetValues() => new List<TValue>(_fwdDictionary.Values);
+
+        public void Add(TKey key, TValue value)
+        {
+            try
+            {
+                _fwdDictionary.Add(key, value);
+            }
+            catch (ArgumentException ex)
+            {
+                if (_revDictionary.ContainsKey(value))
+                    throw new ArgumentException(TwoWayDictionaryExceptions.PairAlreadyAdded(key, value), ex);
+
+                if (!_fwdDictionary.TryGetValue(key, out TValue existing))
+                    throw;
+
+                throw new InvalidOperationException(TwoWayDictionaryExceptions.ItemWithSameKey(existing, key, value), ex);
+            }
+
+            try
+            {
+                _revDictionary.Add(value, key);
+            }
+            catch (ArgumentException ex)
+            {
+                _fwdDictionary.Remove(key);
+
+                if (!_revDictionary.TryGetValue(value, out TKey existing))
+                    throw;
+
+                throw new InvalidOperationException(TwoWayDictionaryExceptions.KeyWithSameValue(existing, key, value), ex);
+            }
+        }
+
+        public void RemoveByKey(TKey key)
+        {
+            if (TryGetValue(key, out TValue value))
+            {
+                _fwdDictionary.Remove(key);
+                _revDictionary.Remove(value);
+            }
+        }
+
+        public void RemoveByValue(TValue value)
+        {
+            if (TryGetKey(value, out TKey key))
+            {
+                _fwdDictionary.Remove(key);
+                _revDictionary.Remove(value);
+            }
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            return _fwdDictionary.TryGetValue(key, out value);
+        }
+
+        public bool TryGetKey(TValue value, out TKey key)
+        {
+            return _revDictionary.TryGetValue(value, out key);
+        }
+
+        public bool ContainsValue(TValue value)
+        {
+            return _revDictionary.ContainsKey(value);
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return _fwdDictionary.ContainsKey(key);
+        }
+
+        public void Clear()
+        {
+            _fwdDictionary.Clear();
+            _revDictionary.Clear();
+        }
+
+        public IEnumerable<TValue> Values => _fwdDictionary.Values;
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return _fwdDictionary.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Collections/TwoWayDictionaryExceptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Acolyte.Collections
+{
+    internal static class TwoWayDictionaryExceptions
+    {
+        public static string ItemWithSameKey<TKey, TValue>(TValue existing, TKey key, TValue value)
+        {
+            return $"An item [{existing}] with the same key [{key}] has already been added. New item: [{value}]";
+        }
+
+        public static string KeyWithSameValue<TKey, TValue>(TKey existing, TKey key, TValue value)
+        {
+            return $"A key [{existing}] with the same value [{value}] has already been added. New key: [{key}]";
+        }
+
+        public static string PairAlreadyAdded<TKey, TValue>(TKey key, TValue value)
+        {
+            return $"A pair ([{key}], [{value}]) has already been added.";
+        }
+    }
+}
