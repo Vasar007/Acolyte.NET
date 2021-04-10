6f122fc82140ab086e2fb11f2ca58261618ce393
Vasily Vasilyev <vasar007@yandex.ru>
2021-04-11 01:58:31 +0200

    Features: add object method overloads and additional methods in CounterInt* classes

--- Acolyte.NET/Libraries/Acolyte.CSharp/Threading/CounterInt32.cs
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Threading/CounterInt32.cs
@@ -1,8 +1,9 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 
 namespace Acolyte.Threading
 {
-    public sealed class CounterInt32
+    public sealed class CounterInt32 : IEquatable<CounterInt32>
     {
         private int _counter;
 
@@ -17,6 +18,70 @@ namespace Acolyte.Threading
         public CounterInt32(int startValue)
         {
             _counter = startValue;
+        }
+
+        #region Object Overridden Methods
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Value);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as CounterInt32);
+        }
+
+        #endregion
+
+        #region IEquatable<CounterInt64> Implementation
+
+        /// <inheritdoc />
+        public bool Equals(CounterInt32? other)
+        {
+            if (other is null) return false;
+
+            if (ReferenceEquals(this, other)) return true;
+
+            return Value.Equals(other.Value);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Determines whether two specified instances of <see cref="CounterInt32" /> are equal.
+        /// </summary>
+        /// <param name="left">Left hand side object to compare.</param>
+        /// <param name="right">Right hand side object to compare.</param>
+        /// <returns>
+        /// <see langword="true" /> if values are memberwise equals, <see langword="false" />
+        /// otherwise.
+        /// </returns>
+        public static bool operator ==(CounterInt32? left, CounterInt32? right)
+        {
+            return Equals(left, right);
+        }
+
+        /// <summary>
+        /// Determines whether two specified instances of <see cref="CounterInt32" /> are not equal.
+        /// </summary>
+        /// <param name="left">Left hand side object to compare.</param>
+        /// <param name="right">Right hand side object to compare.</param>
+        /// <returns>
+        /// <see langword="true" /> if values are not memberwise equals, <see langword="false" />
+        /// otherwise.
+        /// </returns>
+        public static bool operator !=(CounterInt32? left, CounterInt32? right)
+        {
+            return !(left == right);
         }
 
         public CounterInt32 Increment()
@@ -39,6 +104,12 @@ namespace Acolyte.Threading
         public int ExchangeDecrement()
         {
             return Interlocked.Exchange(ref _counter, _counter - 1);
+        }
+
+        public CounterInt32 Advance(int value)
+        {
+            Interlocked.Exchange(ref _counter, _counter + value);
+            return this;
         }
 
         public CounterInt32 Reset()
