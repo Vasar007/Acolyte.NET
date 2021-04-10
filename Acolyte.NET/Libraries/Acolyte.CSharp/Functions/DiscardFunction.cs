0556e5fed2e655325f8d28f6ccc0f09de346e164
Vasily Vasilyev <vasar007@yandex.ru>
2021-04-11 01:59:14 +0200

    Features: add discard functions with index parameter

--- Acolyte.NET/Libraries/Acolyte.CSharp/Functions/DiscardFunction.cs
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Functions/DiscardFunction.cs
@@ -17,15 +17,34 @@ namespace Acolyte.Functions
         public static Action<TElement> Action { get; } = _ => { };
 
         /// <summary>
+        /// Action to discard elements of type <typeparamref name="TElement" />.
+        /// The second parameter of the function represents the index of the element.
+        /// </summary>
+        public static Action<TElement, int> ActionWithIndex { get; } = (_, _) => { };
+
+        /// <summary>
         /// Function to discard elements of type <typeparamref name="TElement" />.
         /// </summary>
         public static Func<TElement, TElement?> Func { get; } = _ => default;
+
+        /// <summary>
+        /// Function to discard elements of type <typeparamref name="TElement" />.
+        /// The second parameter of the function represents the index of the element.
+        /// </summary>
+        public static Func<TElement, int, TElement?> FuncWithIndex { get; } = (_, _) => default;
 
         /// <summary>
         /// "Asynchronous" function to discard elements of type <typeparamref name="TElement" />.
         /// </summary>
         public static Func<TElement, Task<TElement?>> FuncAsync { get; } =
             _ => Task.FromResult(default(TElement));
+
+        /// <summary>
+        /// "Asynchronous" function to discard elements of type <typeparamref name="TElement" />.
+        /// The second parameter of the function represents the index of the element.
+        /// </summary>
+        public static Func<TElement, int, Task<TElement?>> FuncWithIndexAsync { get; } =
+            (_, _) => Task.FromResult(default(TElement));
     }
 
     /// <summary>
@@ -42,10 +61,25 @@ namespace Acolyte.Functions
         public static Func<TElement, TReturn?> Func { get; } = _ => default;
 
         /// <summary>
+        /// Function to discard elements of type <typeparamref name="TElement" /> and return
+        /// default value of type <typeparamref name="TReturn" />.
+        /// The second parameter of the function represents the index of the element.
+        /// </summary>
+        public static Func<TElement, int, TReturn?> FuncWithIndex { get; } = (_, _) => default;
+
+        /// <summary>
         /// "Asynchronous" function to discard elements of type <typeparamref name="TElement" /> and
         /// return task with default value of type <typeparamref name="TReturn" />.
         /// </summary>
         public static Func<TElement, Task<TReturn?>> FuncAsync { get; } =
             _ => Task.FromResult(default(TReturn));
+
+        /// <summary>
+        /// "Asynchronous" function to discard elements of type <typeparamref name="TElement" /> and
+        /// return task with default value of type <typeparamref name="TReturn" />.
+        /// The second parameter of the function represents the index of the element.
+        /// </summary>
+        public static Func<TElement, int, Task<TReturn?>> FuncWithIndexAsync { get; } =
+            (_, _) => Task.FromResult(default(TReturn));
     }
 }
