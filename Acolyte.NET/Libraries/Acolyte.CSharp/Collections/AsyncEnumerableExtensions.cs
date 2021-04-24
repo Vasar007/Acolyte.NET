70f5ff5e5977af1a272069bd886556d732b4d10d
Vasily Vasilyev <vasar007@yandex.ru>
2021-04-24 13:41:32 +0200

    Documentation: fix invalid method reference to AsEnumerableAsync

--- Acolyte.NET/Libraries/Acolyte.CSharp/Collections/AsyncEnumerableExtensions.cs
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Collections/AsyncEnumerableExtensions.cs
@@ -12,7 +12,7 @@ namespace Acolyte.Collections
     [Obsolete("Use \"Acolyte.Linq.AsyncEnumerableExtensions\" instead. This class will be remove in next major version.", error: false)]
     public static class AsyncEnumerableExtensions
     {
-        /// <inheritdoc cref="Linq.AsyncEnumerableExtensions.AsEnumerableAsync{T}(IAsyncEnumerable{T})" />
+        /// <inheritdoc cref="Linq.AsyncEnumerableExtensions.AsEnumerableAsync{T}(IAsyncEnumerable{T}, System.Threading.CancellationToken)" />
         [Obsolete("Use \"Acolyte.Linq.AsyncEnumerableExtensions.AsEnumerableAsync\" instead. This method will be removed in next major version.", error: false)]
         public static Task<IEnumerable<T>> AsEnumerable<T>(this IAsyncEnumerable<T> source)
         {
