30bcf2bb7c2c407c43bd417d7436fa83c1182f56
Vasily Vasilyev <vasar007@yandex.ru>
2021-05-16 16:08:51 +0200

    Features: add extensions for Tuple and ValueTuple

---
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Common/TupleExtensions.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Acolyte.Assertions;
+
+namespace Acolyte.Common
+{
+    public static class TupleExtensions
+    {
+        // TODO: add extension methods for tuple of arbitrary length.
+
+        public static IEnumerable<T> ToEnumerable<T>(this Tuple<T> tuple)
+        {
+            tuple.ThrowIfNull(nameof(tuple));
+
+            yield return tuple.Item1;
+        }
+
+        public static IEnumerable<object?> ToObjectEnumerable<T1, T2>(this Tuple<T1, T2> tuple)
+        {
+            tuple.ThrowIfNull(nameof(tuple));
+
+            yield return tuple.Item1;
+            yield return tuple.Item2;
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this Tuple<T, T> tuple)
+        {
+            return tuple.ToObjectEnumerable().Cast<T>();
+        }
+
+        public static IEnumerable<object?> ToObjectEnumerable<T1, T2, T3>(this Tuple<T1, T2, T3> tuple)
+        {
+            tuple.ThrowIfNull(nameof(tuple));
+
+            yield return tuple.Item1;
+            yield return tuple.Item2;
+            yield return tuple.Item3;
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this Tuple<T, T, T> tuple)
+        {
+            return tuple.ToObjectEnumerable().Cast<T>();
+        }
+
+        public static IEnumerable<object?> ToObjectEnumerable<T1, T2, T3, T4>(
+            this Tuple<T1, T2, T3, T4> tuple)
+        {
+            tuple.ThrowIfNull(nameof(tuple));
+
+            yield return tuple.Item1;
+            yield return tuple.Item2;
+            yield return tuple.Item3;
+            yield return tuple.Item4;
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this Tuple<T, T, T, T> tuple)
+        {
+            return tuple.ToObjectEnumerable().Cast<T>();
+        }
+
+        public static IEnumerable<object?> ToObjectEnumerable<T1, T2, T3, T4, T5>(
+            this Tuple<T1, T2, T3, T4, T5> tuple)
+        {
+            tuple.ThrowIfNull(nameof(tuple));
+
+            yield return tuple.Item1;
+            yield return tuple.Item2;
+            yield return tuple.Item3;
+            yield return tuple.Item4;
+            yield return tuple.Item5;
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this Tuple<T, T, T, T, T> tuple)
+        {
+            return tuple.ToObjectEnumerable().Cast<T>();
+        }
+
+        public static IEnumerable<object?> ToObjectEnumerable<T1, T2, T3, T4, T5, T6>(
+            this Tuple<T1, T2, T3, T4, T5, T6> tuple)
+        {
+            tuple.ThrowIfNull(nameof(tuple));
+
+            yield return tuple.Item1;
+            yield return tuple.Item2;
+            yield return tuple.Item3;
+            yield return tuple.Item4;
+            yield return tuple.Item5;
+            yield return tuple.Item6;
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this Tuple<T, T, T, T, T, T> tuple)
+        {
+            return tuple.ToObjectEnumerable().Cast<T>();
+        }
+
+        public static IEnumerable<object?> ToObjectEnumerable<T1, T2, T3, T4, T5, T6, T7>(
+            this Tuple<T1, T2, T3, T4, T5, T6, T7> tuple)
+        {
+            tuple.ThrowIfNull(nameof(tuple));
+
+            yield return tuple.Item1;
+            yield return tuple.Item2;
+            yield return tuple.Item3;
+            yield return tuple.Item4;
+            yield return tuple.Item5;
+            yield return tuple.Item6;
+            yield return tuple.Item7;
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this Tuple<T, T, T, T, T, T, T> tuple)
+        {
+            return tuple.ToObjectEnumerable().Cast<T>();
+        }
+    }
+}
---
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Common/ValueTupleExtensions.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Acolyte.Common
+{
+    public static class ValueTupleExtensions
+    {
+        // TODO: add extension methods for value tuple of arbitrary length.
+
+        public static IEnumerable<T> ToEnumerable<T>(this ValueTuple<T> tuple)
+        {
+            yield return tuple.Item1;
+        }
+
+        public static IEnumerable<object?> ToObjectEnumerable<T1, T2>(this ValueTuple<T1, T2> tuple)
+        {
+            yield return tuple.Item1;
+            yield return tuple.Item2;
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this ValueTuple<T, T> tuple)
+        {
+            return tuple.ToObjectEnumerable().Cast<T>();
+        }
+
+        public static IEnumerable<object?> ToObjectEnumerable<T1, T2, T3>(
+            this ValueTuple<T1, T2, T3> tuple)
+        {
+
+            yield return tuple.Item1;
+            yield return tuple.Item2;
+            yield return tuple.Item3;
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this ValueTuple<T, T, T> tuple)
+        {
+            return tuple.ToObjectEnumerable().Cast<T>();
+        }
+
+        public static IEnumerable<object?> ToObjectEnumerable<T1, T2, T3, T4>(
+            this ValueTuple<T1, T2, T3, T4> tuple)
+        {
+            yield return tuple.Item1;
+            yield return tuple.Item2;
+            yield return tuple.Item3;
+            yield return tuple.Item4;
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this ValueTuple<T, T, T, T> tuple)
+        {
+            return tuple.ToObjectEnumerable().Cast<T>();
+        }
+
+        public static IEnumerable<object?> ToObjectEnumerable<T1, T2, T3, T4, T5>(
+            this ValueTuple<T1, T2, T3, T4, T5> tuple)
+        {
+            yield return tuple.Item1;
+            yield return tuple.Item2;
+            yield return tuple.Item3;
+            yield return tuple.Item4;
+            yield return tuple.Item5;
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this ValueTuple<T, T, T, T, T> tuple)
+        {
+            return tuple.ToObjectEnumerable().Cast<T>();
+        }
+
+        public static IEnumerable<object?> ToObjectEnumerable<T1, T2, T3, T4, T5, T6>(
+            this ValueTuple<T1, T2, T3, T4, T5, T6> tuple)
+        {
+            yield return tuple.Item1;
+            yield return tuple.Item2;
+            yield return tuple.Item3;
+            yield return tuple.Item4;
+            yield return tuple.Item5;
+            yield return tuple.Item6;
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this ValueTuple<T, T, T, T, T, T> tuple)
+        {
+            return tuple.ToObjectEnumerable().Cast<T>();
+        }
+
+        public static IEnumerable<object?> ToObjectEnumerable<T1, T2, T3, T4, T5, T6, T7>(
+            this ValueTuple<T1, T2, T3, T4, T5, T6, T7> tuple)
+        {
+            yield return tuple.Item1;
+            yield return tuple.Item2;
+            yield return tuple.Item3;
+            yield return tuple.Item4;
+            yield return tuple.Item5;
+            yield return tuple.Item6;
+            yield return tuple.Item7;
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this ValueTuple<T, T, T, T, T, T, T> tuple)
+        {
+            return tuple.ToObjectEnumerable().Cast<T>();
+        }
+    }
+}
