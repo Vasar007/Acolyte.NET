2e3d8e6363d22938d35b358ef65d735f0ec412f1
Vasily Vasilyev <vasar007@yandex.ru>
2021-05-23 19:51:39 +0200

    Feature: add exit codes for F#

---
+++ Acolyte.NET/Libraries/Acolyte.FSharp/Functional/ExitCodes.fs
@@ -0,0 +1,14 @@
+﻿module Acolyte.Functional.ExitCodes
+
+[<Struct>]
+type public ExitCode =
+    | Success
+    | Fail
+
+let public convertExitCode exitCode =
+    match exitCode with
+        | Success -> 0
+        | Fail -> -1
+
+let public successExitCode = convertExitCode ExitCode.Fail
+let public failExitCode = convertExitCode ExitCode.Fail
