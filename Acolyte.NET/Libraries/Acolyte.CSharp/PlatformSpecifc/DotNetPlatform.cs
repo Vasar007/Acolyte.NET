7ebdfb182ffe1d43fc85864539b4538dc8f7a0cd
Vasily Vasilyev <vasar007@yandex.ru>
2021-06-24 14:56:00 +0200

    Features: add .NET platform helper

---
+++ Acolyte.NET/Libraries/Acolyte.CSharp/PlatformSpecifc/DotNetPlatform.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Acolyte.PlatformSpecifc
+{
+    [Flags]
+    public enum DotNetPlatform : ulong
+    {
+        Unknown = 0,
+
+        NetFramework11 = 1,
+        NetFramework20 = NetFramework11 << 1,
+        NetFramework35 = NetFramework20 << 1,
+        NetFramework40 = NetFramework35 << 1,
+        NetFramework403 = NetFramework40 << 1,
+        NetFramework45 = NetFramework403 << 1,
+        NetFramework451 = NetFramework45 << 1,
+        NetFramework452 = NetFramework451 << 1,
+        NetFramework46 = NetFramework452 << 1,
+        NetFramework461 = NetFramework46 << 1,
+        NetFramework462 = NetFramework461 << 1,
+        NetFramework47 = NetFramework462 << 1,
+        NetFramework471 = NetFramework47 << 1,
+        NetFramework472 = NetFramework471 << 1,
+        NetFramework48 = NetFramework472 << 1,
+        NetFramework = NetFramework11 |
+                          NetFramework20 |
+                          NetFramework35 |
+                          NetFramework40 | NetFramework403 |
+                          NetFramework45 | NetFramework451 | NetFramework452 |
+                          NetFramework46 | NetFramework461 | NetFramework462 |
+                          NetFramework47 | NetFramework471 | NetFramework472 |
+                          NetFramework48,
+
+        NetStandard10 = NetFramework48 << 1,
+        NetStandard11 = NetStandard10 << 1,
+        NetStandard12 = NetStandard11 << 1,
+        NetStandard13 = NetStandard12 << 1,
+        NetStandard14 = NetStandard13 << 1,
+        NetStandard15 = NetStandard14 << 1,
+        NetStandard16 = NetStandard15 << 1,
+        NetStandard20 = NetStandard16 << 1,
+        NetStandard21 = NetStandard20 << 1,
+        NetStandard = NetStandard10 | NetStandard11 | NetStandard12 | NetStandard13 | NetStandard14 | NetStandard15 | NetStandard16 |
+                          NetStandard20 | NetStandard21,
+
+        NetCore10 = NetStandard21 << 1,
+        NetCore11 = NetCore10 << 1,
+        NetCore20 = NetCore11 << 1,
+        NetCore21 = NetCore20 << 1,
+        NetCore22 = NetCore21 << 1,
+        NetCore30 = NetCore22 << 1,
+        NetCore31 = NetCore30 << 1,
+        NetCore = NetCore10 | NetCore11 |
+                          NetCore20 | NetCore21 | NetCore22 |
+                          NetCore30 | NetCore31,
+
+        Net5 = NetCore31 << 1,
+        Net6 = Net5 << 1,
+        NetX = Net5 |
+                          Net6,
+    }
+}
---
+++ Acolyte.NET/Libraries/Acolyte.CSharp/PlatformSpecifc/DotNetPlatformHelper.cs
@@ -0,0 +1,114 @@
+﻿namespace Acolyte.PlatformSpecifc
+{
+    public static class DotNetPlatformHelper
+    {
+        public static bool IsNetFramework()
+        {
+            DotNetPlatform dotNetPlatform = GetPlatform();
+            return dotNetPlatform is <= DotNetPlatform.NetFramework11 and <= DotNetPlatform.NetFramework;
+        }
+
+        public static bool IsNetStandard()
+        {
+            DotNetPlatform dotNetPlatform = GetPlatform();
+            return dotNetPlatform is <= DotNetPlatform.NetStandard10 and <= DotNetPlatform.NetStandard;
+        }
+
+        public static bool IsNetCore()
+        {
+            DotNetPlatform dotNetPlatform = GetPlatform();
+            return dotNetPlatform is <= DotNetPlatform.NetCore10 and <= DotNetPlatform.NetCore;
+        }
+
+        public static bool IsNetX()
+        {
+            DotNetPlatform dotNetPlatform = GetPlatform();
+            return dotNetPlatform is <= DotNetPlatform.Net5 and <= DotNetPlatform.NetX;
+        }
+
+        public static DotNetPlatform GetPlatform()
+        {
+#if NETFRAMEWORK1_1
+            return DotNetPlatform.NetFramework11;
+#elif NETFRAMEWORK2_0
+            return DotNetPlatform.NetFramework20;
+#elif NETFRAMEWORK3_5
+            return DotNetPlatform.NetFramework35;
+#elif NETFRAMEWORK4_0
+            return DotNetPlatform.NetFramework40;
+#elif NETFRAMEWORK4_0_3
+            return DotNetPlatform.NetFramework403;
+#elif NETFRAMEWORK4_5
+            return DotNetPlatform.NetFramework45;
+#elif NETFRAMEWORK4_5_1
+            return DotNetPlatform.NetFramework451;
+#elif NETFRAMEWORK4_5_2
+            return DotNetPlatform.NetFramework452;
+#elif NETFRAMEWORK4_6
+            return DotNetPlatform.NetFramework46;
+#elif NETFRAMEWORK4_6_1
+            return DotNetPlatform.NetFramework461;
+#elif NETFRAMEWORK4_6_2
+            return DotNetPlatform.NetFramework462;
+#elif NETFRAMEWORK4_7
+            return DotNetPlatform.NetFramework47;
+#elif NETFRAMEWORK4_7_1
+            return DotNetPlatform.NetFramework471;
+#elif NETFRAMEWORK4_7_2
+            return DotNetPlatform.NetFramework472;
+#elif NETFRAMEWORK4_8
+            return DotNetPlatform.NetFramework48;
+#elif NETFRAMEWORK
+            return DotNetPlatform.NetFramework;
+
+#elif NETSTANDARD1_0
+            return DotNetPlatform.NetStandard10;
+#elif NETSTANDARD1_1
+            return DotNetPlatform.NetStandard11;
+#elif NETSTANDARD1_2
+            return DotNetPlatform.NetStandard12;
+#elif NETSTANDARD1_3
+            return DotNetPlatform.NetStandard13;
+#elif NETSTANDARD1_4
+            return DotNetPlatform.NetStandard14;
+#elif NETSTANDARD1_5
+            return DotNetPlatform.NetStandard15;
+#elif NETSTANDARD1_6
+            return DotNetPlatform.NetStandard16;
+#elif NETSTANDARD2_0
+            return DotNetPlatform.NetStandard20;
+#elif NETSTANDARD2_1
+            return DotNetPlatform.NetStandard21;
+#elif NETSTANDARD
+            return DotNetPlatform.NetStandard;
+
+#elif NETCORE1_0
+            return DotNetPlatform.NetCore10;
+#elif NETCORE1_1
+            return DotNetPlatform.NetCore11;
+#elif NETCORE2_0
+            return DotNetPlatform.NetCore20;
+#elif NETCORE2_1
+            return DotNetPlatform.NetCore21;
+#elif NETCORE2_2
+            return DotNetPlatform.NetCore22;
+#elif NETCORE3_0
+            return DotNetPlatform.NetCore30;
+#elif NETCORE3_1
+            return DotNetPlatform.NetCore31;
+#elif NETCORE
+            return DotNetPlatform.NetCore;
+
+#elif NET5
+            return DotNetPlatform.Net5;
+#elif NET6
+            return DotNetPlatform.Net6;
+#elif NETX
+            return DotNetPlatform.NetX;
+
+#else
+            return DotNetPlatform.Unknown;
+#endif
+        }
+    }
+}
