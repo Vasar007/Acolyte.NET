ebe922305d03aa510dafcd00548bf062328c3788
Vasily Vasilyev <vasar007@yandex.ru>
2021-08-15 18:20:35 +0200

    Features: add disposable scope and console tools in Acolyte.CSharp package

---
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Collections/DisposableScope.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Acolyte.Collections
+{
+    public sealed class DisposableScope : IDisposable
+    {
+        private readonly Stack<IDisposable> _disposables;
+
+
+        public DisposableScope()
+            : this(capacity: 4)
+        {
+        }
+
+        public DisposableScope(
+            int capacity)
+        {
+            _disposables = new Stack<IDisposable>(capacity);
+        }
+
+        public T Capture<T>(T disposable)
+            where T : IDisposable
+        {
+            _disposables.Push(disposable);
+            return disposable;
+        }
+
+        public void ReleaseAll()
+        {
+            _disposables.Clear();
+        }
+
+        #region IDisposable Implementation
+
+        private bool _disposed;
+
+        public void Dispose()
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            foreach (IDisposable disposable in _disposables)
+            {
+                try
+                {
+                    disposable.Dispose();
+                }
+                catch (Exception)
+                {
+                    // Ignore.
+                }
+            }
+
+            _disposed = true;
+        }
+
+        #endregion
+    }
+}
