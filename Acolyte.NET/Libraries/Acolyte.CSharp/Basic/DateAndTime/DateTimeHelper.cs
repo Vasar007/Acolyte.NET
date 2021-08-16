a85f2a740a9c6dbcb077fd8975fcd314ee3d18e1
Vasily Vasilyev <vasar007@yandex.ru>
2021-08-16 11:05:54 +0200

    Features: add basic helper for DateTime (need to move more classes into basic folder)

---
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Basic/DateAndTime/DateTimeHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Acolyte.Basic.DateAndTime
+{
+    public static class DateTimeHelper
+    {
+        public static string SimpleFormat { get; } = "dd/MM/yyyy HH:mm:ss";
+
+
+        public static string ToSimpleString(this DateTime dateTime)
+        {
+            return dateTime.ToString(SimpleFormat);
+        }
+
+        public static string ToLocalSimpleString(this DateTime dateTime)
+        {
+            return dateTime.ToLocalTime().ToSimpleString();
+        }
+    }
+}
