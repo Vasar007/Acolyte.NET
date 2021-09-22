d5f7fcc214ba4ae8b75b59be0d382e7c67a93a8f
Vasily Vasilyev <vasar007@yandex.ru>
2021-09-22 12:41:16 +0200

    Features: add NameGenerator

---
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Common/NameGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using Acolyte.Assertions;
+
+namespace Acolyte.Common
+{
+    public static class NameGenerator
+    {
+        public static string MakeUniqueFromHash(string? prefix, object obj)
+        {
+            obj.ThrowIfNull(nameof(obj));
+
+            int hashCode = obj.GetHashCode();
+            string hashCodeAsString = hashCode.ToString(
+                NumberFormatInfo.InvariantInfo
+            );
+
+            return $"{prefix}{hashCodeAsString}";
+        }
+    }
+}
