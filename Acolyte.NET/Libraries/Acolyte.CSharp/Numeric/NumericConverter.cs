a620f4e6c7ec0788747d1e81b103ea9f8045951f
Vasily Vasilyev <vasar007@yandex.ru>
2021-09-22 12:19:43 +0200

    Features, refactoring: add more logic for Numeric (BigInteger, NumericConverter) + move extension methods to inner subfolders

---
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Numeric/NumericConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Acolyte.Numeric
+{
+    public static class NumericConverter
+    {
+        public static int ConvertToInt32Safe(uint value)
+        {
+            return ConvertToInt32Safe(value, defaultValue: int.MaxValue);
+        }
+
+        public static int ConvertToInt32Safe(uint value, int defaultValue)
+        {
+            return ConvertInternalSafe(value, x => Convert.ToInt32(x), defaultValue);
+        }
+
+        public static int ConvertToInt32Safe(ulong value)
+        {
+            return ConvertToInt32Safe(value, defaultValue: int.MaxValue);
+        }
+
+        public static int ConvertToInt32Safe(ulong value, int defaultValue)
+        {
+            return ConvertInternalSafe(value, x => Convert.ToInt32(x), defaultValue);
+        }
+
+        public static long ConvertToInt64Safe(ulong value)
+        {
+            return ConvertToInt64Safe(value, defaultValue: long.MaxValue);
+        }
+
+        public static long ConvertToInt64Safe(ulong value, long defaultValue)
+        {
+            return ConvertInternalSafe(value, x => Convert.ToInt64(x), defaultValue);
+        }
+
+        private static TOut ConvertInternalSafe<TIn, TOut>(TIn value, Func<TIn, TOut> conversion, TOut defaultValue)
+            where TIn : struct
+        {
+            try
+            {
+                return conversion(value);
+            }
+            catch (OverflowException)
+            {
+                return defaultValue;
+            }
+        }
+    }
+}
