12061a0492d9a07ce4beaeb57f193fc5c38008bb
Vasily Vasilyev <vasar007@yandex.ru>
2021-10-22 12:22:00 +0200

    Features: add Lambda comparer and equality comparer

---
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Collections/Comparers/LambdaComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Acolyte.Assertions;
+
+namespace Acolyte.Collections.Comparers
+{
+    public sealed class LambdaComparer<T> : IComparer<T>
+    {
+        private readonly Func<T, T, int> _lambdaComparer;
+
+
+        public LambdaComparer(
+            Func<T, T, int> lambdaComparer)
+        {
+            _lambdaComparer = lambdaComparer.ThrowIfNull(nameof(lambdaComparer));
+        }
+
+        #region Implementation of IComparer<T>
+
+        public int Compare(T x, T y)
+        {
+            return _lambdaComparer(x, y);
+        }
+
+        #endregion
+    }
+}
---
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Collections/Comparers/LambdaEqualityComparer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Acolyte.Assertions;
+
+namespace Acolyte.Collections.Comparers
+{
+    public sealed class LambdaEqualityComparer<T> : IEqualityComparer<T>
+    {
+        private readonly Func<T, T, bool> _lambdaComparer;
+        private readonly Func<T, int> _lambdaHash;
+
+
+        public LambdaEqualityComparer(
+            Func<T, T, bool> lambdaComparer,
+            Func<T, int> lambdaHash)
+        {
+            _lambdaComparer = lambdaComparer.ThrowIfNull(nameof(lambdaComparer));
+            _lambdaHash = lambdaHash.ThrowIfNull(nameof(lambdaHash));
+        }
+
+        public static LambdaEqualityComparer<T> Create(
+            Func<T, T, bool> lambdaComparer)
+        {
+            return new LambdaEqualityComparer<T>(
+                lambdaComparer: lambdaComparer,
+                lambdaHash: _ => 0
+            );
+        }
+
+        #region Implementation of IEqualityComparer<T>
+
+        public bool Equals(T x, T y)
+        {
+            return _lambdaComparer(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return _lambdaHash(obj);
+        }
+
+        #endregion
+    }
+}
