459f9a2a5b92de9c4ec2444c54cb74135156a9e5
Vasily Vasilyev <vasar007@yandex.ru>
2021-10-22 12:21:00 +0200

    BREAKING CHANGE: move InverseComparer to Comparers namespace

---
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Collections/Comparers/InverseComparer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Acolyte.Collections.Comparers
+{
+    /// <summary>
+    /// Allows to invert every object of <see cref="IComparer{T}" /> type.
+    /// </summary>
+    /// <typeparam name="T">The type of objects to compare.</typeparam>
+    public sealed class InverseComparer<T> : IComparer<T>
+    {
+        private readonly IComparer<T> _comparer;
+
+
+        public InverseComparer(
+            IComparer<T>? comparer)
+        {
+            _comparer = comparer ?? Comparer<T>.Default;
+        }
+
+        #region IComparer<T> Implementation
+
+        public int Compare(T obj1, T obj2)
+        {
+            int comparison = _comparer.Compare(obj1, obj2);
+            return -comparison;
+        }
+
+        #endregion
+    }
+}
--- Acolyte.NET/Libraries/Acolyte.CSharp/Linq/Max.cs
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Linq/Max.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using Acolyte.Collections;
+using Acolyte.Collections.Comparers;
 
 namespace Acolyte.Linq
 {
