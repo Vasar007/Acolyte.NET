877c60a5989be44a305826ccfff2433b4bbfb08a
Vasily Vasilyev <vasar007@yandex.ru>
2021-12-20 14:24:13 +0100

    Features: add Collapse method to LINQ extensions

---
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Linq/Collapse.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Acolyte.Linq
+{
+    public static partial class EnumerableExtensions
+    {
+        /// <summary>
+        /// Collapses a series of sequences down by using items from the first sequence until it
+        /// finishes, then continuing from the same index through the second sequence, and so on
+        /// until all sequences have been exhausted.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of the input sequences.</typeparam>
+        /// <param name="sequences">The input sequences.</param>
+        /// <returns>
+        /// Items from each sequence in turn, yielding those from the first sequence first.
+        /// </returns>
+        public static IEnumerable<T> Collapse<T>(this IEnumerable<IEnumerable<T>> sequences)
+        {
+            int position = 0;
+            foreach (IEnumerable<T> sequence in sequences)
+            {
+                foreach (T obj in sequence.Skip(position))
+                {
+                    ++position;
+                    yield return obj;
+                }
+            }
+        }
+    }
+}
---
+++ Acolyte.NET/Tests/Acolyte.CSharp.Tests/Linq/CollapseTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Acolyte.Tests.Linq
+{
+    public sealed class CollapseTests
+    {
+        public CollapseTests()
+        {
+        }
+
+        #region Null Values
+
+        #endregion
+
+        #region Empty Values
+
+        #endregion
+
+        #region Predefined Values
+
+        #endregion
+
+        #region Some Values
+
+        #endregion
+
+        #region Random Values
+
+        #endregion
+
+        #region Extended Logical Coverage
+
+        #endregion
+    }
+}
