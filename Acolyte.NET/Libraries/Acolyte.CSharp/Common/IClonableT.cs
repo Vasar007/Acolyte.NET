b993d7733bc56c187405affbba32901f42ab0f6a
Vasily Vasilyev <vasar007@yandex.ru>
2022-01-14 12:01:49 +0100

    BREAKING CHANGE: ICloneable<T> inherits ICloneable like IEnumerable<T>

--- Acolyte.NET/Libraries/Acolyte.CSharp/Common/IClonableT.cs
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Common/IClonableT.cs
@@ -1,15 +1,17 @@
-﻿namespace Acolyte.Common
+﻿using System;
+
+namespace Acolyte.Common
 {
     /// <summary>
     /// Provides generic interface to clone objects.
     /// </summary>
     /// <typeparam name="T">The type of the object to clone.</typeparam>
-    public interface ICloneable<T>
+    public interface ICloneable<T> : ICloneable
     {
         /// <summary>
         /// Creates a new object that is a copy of the current instance.
         /// </summary>
         /// <returns>A new object that is a copy of this instance.</returns>
-        T Clone();
+        new T Clone();
     }
 }
