f6e5b06ad8605e963a0fd14ae644658cfa99f215
Vasily Vasilyev <vasar007@yandex.ru>
2022-02-11 19:21:05 +0100

    Feature: add extensions for IConvertable

---
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Common/Extensions/ExtensionMethodsConvertible.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Acolyte.Assertions;
+
+namespace Acolyte.Common.Extensions
+{
+    public static class ExtensionMethodsConvertible
+    {
+        public static bool HasFlag<TConvertible>(this TConvertible value, TConvertible flag)
+           where TConvertible : IConvertible
+        {
+            value.ThrowIfNullValue(nameof(value), assertOnPureValueTypes: false);
+            flag.ThrowIfNullValue(nameof(flag), assertOnPureValueTypes: false);
+
+            // Get the type code of the input value.
+            TypeCode typeCode = value.GetTypeCode();
+
+            // If the underlying type of the flag is signed.
+            if (typeCode == TypeCode.SByte || typeCode == TypeCode.Int16 || typeCode == TypeCode.Int32 || typeCode == TypeCode.Int64)
+                return (Convert.ToInt64(value) & Convert.ToInt64(flag)) != 0;
+
+            // If the underlying type of the flag is unsigned.
+            if (typeCode == TypeCode.Byte || typeCode == TypeCode.UInt16 || typeCode == TypeCode.UInt32 || typeCode == TypeCode.UInt64)
+                return (Convert.ToUInt64(value) & Convert.ToUInt64(flag)) != 0;
+
+            // Unsupported flag type.
+            throw new NotSupportedException($"The comparison of the type {flag.GetType().Name} is not implemented.");
+        }
+    }
+}
