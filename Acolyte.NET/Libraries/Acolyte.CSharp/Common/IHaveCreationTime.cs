67cf0c2a7e66159e988fb004cbbc33eac1588798
Vasily Vasilyev <vasar007@yandex.ru>
2022-05-14 15:09:27 +0200

    BREAKING CHANGES: IHaveCreationTime.CreationTime -> CreationTimeUtc

--- Acolyte.NET/Libraries/Acolyte.CSharp/Common/IHaveCreationTime.cs
+++ Acolyte.NET/Libraries/Acolyte.CSharp/Common/IHaveCreationTime.cs
@@ -3,13 +3,13 @@
 namespace Acolyte.Common
 {
     /// <summary>
-    /// Provides read-only <see cref="DateTime" /> property to store creation time.
+    /// Provides read-only <see cref="DateTime" /> property to store creation time in UTC format.
     /// </summary>
     public interface IHaveCreationTime
     {
         /// <summary>
         /// Creation time of the object.
         /// </summary>
-        DateTime CreationTime { get; }
+        DateTime CreationTimeUtc { get; }
     }
 }
